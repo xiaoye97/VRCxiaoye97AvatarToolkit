1aee8e04328cc95756f6d2157967d6218fc79c70
宵夜 <34893504+xiaoye97@users.noreply.github.com>
2025-01-01 19:02:00 +0800

    lilToon亮度修改工具更新

--- Packages/com.xiaoye97.avatar-toolkit/Editor/lilToonLightingSettingTool.cs
+++ Packages/com.xiaoye97.avatar-toolkit/Editor/lilToonLightingSettingTool.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -23,33 +24,68 @@ namespace xiaoye97.AvatarToolkit
             LowerBrightnessLimit = EditorGUILayout.Slider("亮度下限", LowerBrightnessLimit, 0, 1);
             UpperBrightnessLimit = EditorGUILayout.Slider("亮度上限", UpperBrightnessLimit, 0, 10);
             MonochromeLighting = EditorGUILayout.Slider("单色照明", MonochromeLighting, 0, 1);
-            EnviromentStrengthOnShadowColor = EditorGUILayout.Slider("阴影颜色的环境强度", EnviromentStrengthOnShadowColor, 0, 1);
+            EnviromentStrengthOnShadowColor =
+                EditorGUILayout.Slider("阴影颜色的环境强度", EnviromentStrengthOnShadowColor, 0, 1);
             if (GUILayout.Button("应用到所有lilToon材质球"))
             {
                 ApplyToAllMaterial();
             }
         }
-        
+
         private void ApplyToAllMaterial()
         {
-            Material[] materials = Resources.FindObjectsOfTypeAll<Material>();
-            foreach (Material material in materials)
+            List<PathMaterial> pathMaterials = new List<PathMaterial>();
+            var paths = AssetDatabase.GetAllAssetPaths();
+            foreach (var path in paths)
             {
-                if (material.shader.name == "lilToon" || material.shader.name.StartsWith("_lil"))
+                var type = AssetDatabase.GetMainAssetTypeAtPath(path);
+                if (type == typeof(Material))
                 {
-                    ApplyToMaterial(material);
+                    var material = AssetDatabase.LoadAssetAtPath<Material>(path);
+                    string shaderName = material.shader.name;
+                    if (shaderName == "lilToon" || shaderName.StartsWith("_lil") || shaderName.StartsWith("Hidden/lil"))
+                    {
+                        var pm = ApplyToMaterial(material);
+                        pathMaterials.Add(pm);
+                    }
                 }
             }
+
+            pathMaterials.Sort((a, b) => string.Compare(a.Path, b.Path, StringComparison.Ordinal));
+            foreach (var pathMaterial in pathMaterials)
+            {
+                EditorUtility.SetDirty(pathMaterial.Material);
+                Debug.Log(
+                    $"修改了[{pathMaterial.Material.name}]({pathMaterial.Material.shader.name})的lilToon参数, 路径:{pathMaterial.Path}",
+                    pathMaterial.Material);
+            }
+
+            Debug.Log($"共修改了{pathMaterials.Count}个lilToon材质球");
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
         }
-        
-        private void ApplyToMaterial(Material material)
+
+        private PathMaterial ApplyToMaterial(Material material)
         {
             string path = AssetDatabase.GetAssetPath(material);
             material.SetFloat("_LightMinLimit", LowerBrightnessLimit);
             material.SetFloat("_LightMaxLimit", UpperBrightnessLimit);
             material.SetFloat("_MonochromeLighting", MonochromeLighting);
             material.SetFloat("_ShadowEnvStrength", EnviromentStrengthOnShadowColor);
-            Debug.Log($"修改了[{material.name}]({material.shader.name})的lilToon参数, 路径:{path}", material);
+            return new PathMaterial(path, material);
+        }
+
+        public class PathMaterial
+        {
+            public string Path;
+            public Material Material;
+
+            public PathMaterial(string path, Material material)
+            {
+                Path = path;
+                Material = material;
+            }
         }
     }
 }
